c54bd393db3c94d203fc5290b7d322b498327846
Mila <carivera14@espe.edu.ec>
2023-05-11 12:19:09 -0500

    Documentos
    Documentos

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Home from "./pages/Home";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route index element={<Home/>} />
+        </Routes>    
+      </Router>
+    </div>
+  );
+}
+
+export default App;
