6b159146e6447ca4012ad45ff6d9ea5a4174526f
StevenJossueAchig <stevenjossue5@gmail.com>
2023-05-16 20:07:39 -0500

    Traje

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {Carousel} from "./pages/Carousel"
 import "./styles/carrusel.css"
-import Home from "./pages/Traje";
-import Preguntas from "./pages/preguntas/Preguntas.js"
 import Traje from "./pages/Traje";
+import Preguntas from "./pages/preguntas/Preguntas.js"
 
 
 function App() {
@@ -17,11 +16,9 @@ function App() {
         <Routes>
           <Route index element={<Carousel/>} />
 
-          <Route path="/home" element={<Home />} />
+          <Route path="/traje" element={<Traje />} />
 
           <Route path='/ronda_preguntas' element={<Preguntas/>}/>
-          
-          <Route path='/traje' element={<Traje/>}/>
           
         </Routes>    
       </Router>
