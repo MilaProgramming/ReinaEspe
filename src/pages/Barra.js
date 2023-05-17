01f2051d39e917a645e0358899310e02b5cda155
StevenJossueAchig <stevenjossue5@gmail.com>
2023-05-16 20:58:15 -0500

    Barra

--- src/pages/Barra.js
+++ src/pages/Barra.js
@@ -2,16 +2,17 @@ import React from 'react'
 const imagen = require.context("../img/");
 
 const Barra = () => (
-  <div className="container_cont">
-      <div className="barras">
-          <h2>Etapa 3 Calificacion de Barras</h2>
-          <div className="barra">
-              <img className="circle-image" src={(imagen("./reina.jpg"))} alt="" />
-              <h2>Califique la barra del departamento</h2>
-          </div>
-      </div>
-      
-  </div>
+    <div className="container_cont">
+        <div className="barras">
+            <h2>Etapa 3 Calificacion de Barras</h2>
+            <img className="circleGreen" src={(imagen("./circleVo.png"))} alt="" />
+            <div className="barra">
+                <img className="circle-image" src={(imagen("./reina.jpg"))} alt="" />
+                <h2>Califique la barra del departamento</h2>
+            </div>
+        </div>
+
+    </div>
 );
 
 export default Barra;
