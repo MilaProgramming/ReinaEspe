efe478f874be09bb04c11b4e857b4233086bfbd7
Kevin Vargas <70618510+KernyMC@users.noreply.github.com>
2023-05-20 15:56:27 -0500

    Add files via upload

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import "./Login.css";
+import { FaUser, FaLock } from 'react-icons/fa';
 
 function Login() {
   return (
-    <div>Homeeeeeeeeeee</div>
-  )
+    <div className="Login">
+<div className="contenedorPrincipal">
+      <div className="loginContainer">
+          <div className="frmpantnegra">
+            <img
+              className="logoespereina-icon"
+              alt=""
+              src="/logoespereina.png"
+            />
+          </div>
+
+
+        <div>
+        <h1 className="iniciar-sesin">Iniciar Sesión
+        </h1>
+        </div>
+      <div className="contenedorFormulario">
+        <div className="input-container">
+        
+          <label> 
+            Usuario <span className="espacio"></span>  
+            <span className="icono"><FaUser /></span></label>
+          <input type="text" placeholder="Usuario" name="uname" required />
+          {/* {renderErrorMessage("uname")} */}
+        </div>
+        <div className="input-container">
+        
+          <label>
+            Contraseña <span className="espacio"></span>
+          <span className="icono"><FaLock /></span>
+          </label>
+          <input type="password"  placeholder="Contraseña" name="pass" required />
+          {/* {renderErrorMessage("pass")} */}
+        </div>
+
+  
+        <button className="loginBut">
+          <p>Iniciar Sesión</p>
+        </button>
+        </div>
+      </div>
+      <div class="rectanguloverde">
+          <img
+            className="siluetareina-icon"
+            alt=""
+            src="/siluetareina.png"
+          />
+      </div>
+    </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
