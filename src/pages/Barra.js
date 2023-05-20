d00ad2b0213f8d16f8beff8d4cfd78882cebba9d
[SantiagoQuishpe312] <[sdquishpe1@espe.edu.ec]>
2023-05-20 11:30:09 -0500

    cambio barras

--- src/pages/Barra.js
+++ src/pages/Barra.js
@@ -2,38 +2,64 @@ import React from 'react'
 import '../styles/Barra.css';
 import CalificationButton from '../components/CalificationButton';
 const imagen = require.context("../img/");
+const imagen2 = require.context("../assets/");
+const data = [
+    { id: 1, sede:"MATRIZ",departamento: "Dpto de Ciencias de la Computacion", nombre: "Maria", foto:"./chica4.jpg" },
+    { id: 2, sede:"LTCGA",departamento: "Dpto de Ciencias Medicas", nombre: "Karelis",foto:"./chica7.jpg" },
+    { id: 3, sede:"MATRIZ",departamento: "Dpto de Mecanica", nombre: "Elizabeth",foto:"./chica2.jpg" },
+    { id: 4, sede:"MATRIZ",departamento: "Dpto de Electronica", nombre: "Josselin",foto:"./chica7.jpg" },
+    { id: 5, sede:"MATRIZ",departamento: "Dpto de Ciencias", nombre: "Mishel", foto:"./chica5.jpg"},
+    { id: 6, sede:"MATRIZ", departamento: "Dpto de Ciencias Ambientales", nombre: "Pamela",foto:"./chica6.jpg" },
+  ];
+
 
 const Barra = () => (
-    <div className="container_cont">
+   <> <div className="container_cont">
         <div className="barras">
             <h2>Etapa 3 Calificacion de Barras</h2>
             <div className="barra">
                 <h2>Califique la barra del departamento</h2>
+                <img src={imagen2("./indicador.png")}/>
             </div>
         </div>
-        <div className="matriz">
-            <h2>Matriz</h2>
-            <img className="circleGreen" src={(imagen("./circleVo.png"))} alt="" />
-            <img className="circleOrange" src={(imagen("./circleO.png"))} alt="" />
-            <div className="sangolqui">
-                <h2>Departamento de Ciencias Medicas</h2>
-
-                <img className="circle-image" src={(imagen("./reina.jpg"))} alt="" />
-            </div>
-            <CalificationButton/>
-        </div>
-        <div className="latacunga">
-            
-            <h2>latacunga</h2>
-            <img className="circleGreenLatacunga" src={(imagen("./circleVo.png"))} alt="" />
-            <img className="circleOrangeLatacguna" src={(imagen("./circleO.png"))} alt="" />
-            <div className="lata">
-                <h2>   Departamento de     Ciencias de la          Computación</h2>
-
-                <img className="circle-image" src={(imagen("./reina.jpg"))} alt="" />
-            </div>
-        </div>
+        
+        
     </div>
+    <div className="container-general">
+    <div className='filas'>
+    
+    { data.map((datos)=>
+    <div className='contenedor-candidatas'>
+    
+    <div className='green-round'>
+    <br/><br/>
+        <h3>{datos.departamento}</h3>
+        <br/>
+        <h3>{datos.nombre}</h3>
+    </div>
+    <div className='orange-round'> 
+    <br/>
+    <h5>{datos.sede}</h5>
+    
+    </div>
+    <img src={imagen2(datos.foto)}></img>
+    <div className='botones-calificacion'><button>
+        1
+        </button> <button>
+        2
+        </button> <button>
+        3
+        </button> <button>
+        4
+        </button> 
+        <button>
+        5
+        </button>   </div>
+    </div>
+    )
+        
+    }</div></div>
+    </>
 );
 
 export default Barra;
