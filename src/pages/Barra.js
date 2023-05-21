f6a1c45eefb368b24171d2c980fa98f72ae12e86
[SantiagoQuishpe312] <[sdquishpe1@espe.edu.ec]>
2023-05-20 20:44:18 -0500

    actualizacion

--- src/pages/Barra.js
+++ src/pages/Barra.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import '../styles/Barra.css';
+import NavBar from '../components/NavBar';
 import CalificationButton from '../components/CalificationButton';
 const imagen2 = require.context("../assets/");
+//para probar datos
 const data = [
-    { id: 1, sede:"MATRIZ",departamento: "Dpto de Ciencias de la Computación", nombre: "Maria", foto:"./chica4.jpg"},
+    { id: 1, sede:"MATRIZ",departamento: "Dpto de Ciencias de la Computación", nombre: "Maria", foto:"./chica7.jpg"},
     { id: 2, sede:"LTCGA",departamento: "Dpto de Ciencias Médicas", nombre: "Karelis",foto:"./chica7.jpg" },
-    { id: 3, sede:"MATRIZ",departamento: "Dpto de Ciencias de la Energia y Mecánica", nombre: "Elizabeth",foto:"./chica2.jpg" },
+    { id: 3, sede:"MATRIZ",departamento: "Dpto de Ciencias de la Energia y Mecánica", nombre: "Elizabeth",foto:"./chica7.jpg" },
     { id: 4, sede:"MATRIZ",departamento: "Dpto de Ciencias de la Tierra y Construcción", nombre: "Josselin",foto:"./chica7.jpg" },
-    { id: 5, sede:"MATRIZ",departamento: "Dpto de Ciencias de la Vida y Agricultura", nombre: "Mishel", foto:"./chica5.jpg"},
+    { id: 5, sede:"MATRIZ",departamento: "Dpto de Ciencias de la Vida y Agricultura", nombre: "Mishel", foto:"./chica7.jpg"},
     { id: 6, sede:"MATRIZ", departamento: "Dpto de Ciencias Económicas, Administrativas", nombre: "Pamela",foto:"./chica7.jpg" },
-    { id: 7, sede:"MATRIZ", departamento: "Dpto de Ciencias Humanas y Sociales", nombre: "Pamela",foto:"./chica6.jpg" },
+    { id: 7, sede:"MATRIZ", departamento: "Dpto de Ciencias Humanas y Sociales", nombre: "Pamela",foto:"./chica7.jpg" },
     { id: 8, sede:"MATRIZ", departamento: "Dpto de Seguridad y Defensa", nombre: "FRANCISCA",foto:"./chica7.jpg" },
-    { id: 6, sede:"MATRIZ", departamento: "Dpto de Electrica, Electrónica y Teliecomunicaciones", nombre: "DANIELA",foto:"./chica1.jpg" },
-    { id: 6, sede:"MATRIZ", departamento: "Dpto de Ciencias Exactas", nombre: "VANESSA",foto:"./chica6.jpg" },
+    { id: 6, sede:"MATRIZ", departamento: "Dpto de Electrica, Electrónica y Teliecomunicaciones", nombre: "DANIELA",foto:"./chica7.jpg" },
+    { id: 6, sede:"MATRIZ", departamento: "Dpto de Ciencias Exactas", nombre: "VANESSA",foto:"./chica7.jpg" },
     { id: 6, sede:"MATRIZ", departamento: "Dpto de Ciencias ------------", nombre: "NAYELI",foto:"./chica7.jpg" },
 ];
 
 
 
 const Barra = () => (
-   <> <div className="container_cont">
+   <> 
+   <NavBar texto="Calificación de Barras"/> {/*Cambio de texto de texto de header*/}
+   <div className="container_cont">
         <div className="barras">
-            <h2>Etapa 3 Calificación de Barras</h2>
+            
             <div className="barra">
                 <h2>Califique la barra del departamento</h2>
                 <img src={imagen2("./indicador.png")}/>
